c7511cef26326b115a5837181460df0df5557f8f
Calvin Tan <hello@calvin-tan.com>
2019-08-24 21:29:33 +0800

    Add google map for displaying flat markers

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
   render() {
+    let marker = null;
+    let center = {
+      lat: 48.8566,
+      lng: 2.3522
+    };
+
     const style = {
       height: "100vh"
     };
 
     return (
       <div className="col-sm-5" style={style}>
-        map
+        <GoogleMapReact center={center} defaultZoom={11}>
+          {marker}
+        </GoogleMapReact>
       </div>
     );
   }
