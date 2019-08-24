e20da86a681b1accc65dbe5b99c5406663cbbb28
Calvin Tan <hello@calvin-tan.com>
2019-08-24 22:42:18 +0800

    Add a marker to the map to display the location of a selected flat

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
 
 class Map extends Component {
   render() {
+    const style = {
+      width: "20px",
+      height: "20px",
+      backgroundColor: "red",
+      borderRadius: "50%"
+    };
+
     let marker = null;
     let center = {
       lat: 48.8566,
       lng: 2.3522
     };
 
-    const style = {
-      height: "100vh"
-    };
+    if (this.props.selectedFlat) {
+      marker = (
+        <div
+          style={style}
+          lat={this.props.selectedFlat.lat}
+          lng={this.props.selectedFlat.lng}
+        />);
+      center = {
+        lat: this.props.selectedFlat.lat,
+        lng: this.props.selectedFlat.lng
+      };
+    }
 
     return (
-      <div className="col-sm-5" style={style}>
+      <div className="col-sm-5 map-container">
         <GoogleMapReact center={center} defaultZoom={11}>
           {marker}
         </GoogleMapReact>
@@ -23,4 +40,8 @@ class Map extends Component {
   }
 }
 
-export default Map;
+function mapStateToProps(state) {
+  return { selectedFlat: state.selectedFlat };
+}
+
+export default connect(mapStateToProps)(Map);
